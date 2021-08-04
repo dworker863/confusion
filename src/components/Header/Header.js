a19ae1765016eb185b7b681f57a6a24d5c1959ba
dworker <dworker863@gmail.com>
2021-08-04 20:52:53 +0600

    Added static Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,29 @@
-import { Navbar, NavbarBrand } from 'reactstrap';
+import { Nav, Navbar, NavbarBrand, NavItem, NavLink } from 'reactstrap';
+import logo from 'assets/images/logo.png';
+import styles from './Header.module.css';
 
 const Header = () => {
   return (
     <header>
-      <Navbar dark color="primary">
+      <Navbar className={styles.navbar} dark expand="md">
         <div className="container">
-          <NavbarBrand href="/">Confusion</NavbarBrand>
+          <Nav className="mr-auto" navbar>
+            <NavbarBrand href="/">
+              <img className={styles.logo} src={logo} alt="Confusion" />
+            </NavbarBrand>
+            <NavItem>
+              <NavLink href="#">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">About Us</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Menu</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Contact Us</NavLink>
+            </NavItem>
+          </Nav>
         </div>
       </Navbar>
     </header>
